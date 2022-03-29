1f98ec61db39bc46b9c8776d352291b7662323c9
200_안심전기3 <200_3@DTRO.LOCAL>
2022-03-29 20:05:06 +0900

    Locale message

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Demo1Application {
@@ -10,4 +15,10 @@ public class Demo1Application {
         SpringApplication.run(Demo1Application.class, args);
     }
 
+    @Bean
+    public LocaleResolver localResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
---
+++ src/main/java/com/example/demo/Helloworld/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Helloworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+    @Autowired
+    private MessageSource messageSource;
+
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false)Locale locale){
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+}
